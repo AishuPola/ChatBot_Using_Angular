3e12e31266ac33996647b58a413fb3bb7d269e46
AishuPola <aishwarya.pola@proclink.com>
2026-03-31 20:11:57 +0530

    created user-managment component

--- chatbot/src/app/shared/components/layout/layout.ts
+++ chatbot/src/app/shared/components/layout/layout.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from '../../../footer/footer';
 import { Header } from '../../../header/header';
+import { Navbar } from '../../../navbar/navbar';
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, Navbar],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
